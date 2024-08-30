fd0feeab9b32c00dfcedf11c84e957bf2419d50d
dineshtdb <dineshtdb@gmail.com>
2024-08-30 19:46:20 +0530

    okay

--- app/page.js
+++ app/page.js
@@ -1,11 +1,11 @@
 'use client'
-import BlogItem from "@/components/BlogItem";
+import BlogList from "@/components/BlogList";
 import Headers from "@/components/Headers"
 export default function Home() {
   return (
    <>
    <Headers/>
-   <BlogItem/>
+   <BlogList/>
    </>
   );
 }
--- components/BlogItem.jsx
+++ components/BlogItem.jsx
@@ -1,14 +1,17 @@
 import React from 'react'
 import Image from 'next/image'
 import { assets, blog_data } from '@/Assets/assets'
-const BlogItem = () => {
+const BlogItem = ({title,description,category,image}) => {
   return (
     <div className='max-w-[330px] sm:max-w-[300px bg-white border border-black hover:shadow-[-7px_7px_0px_#000000]'>
-        <Image src={blog_data[0].image} width={400} height={400} className='border border-black'/>
-        <p className='ml-5 mt-5 px-1 inline-block bg-black text-white text-sm'>{blog_data[0].category}</p>
+        <Image src={image} width={400} height={400} className='border border-black'/>
+        <p className='ml-5 mt-5 px-1 inline-block bg-black text-white text-sm'>{category}</p>
         <div className="p-5">
-            <h5 className='mb-2 tetx-lg font-medium tracking-tight text-grey-900'>{blog_data[0].title}</h5>
-            <p className='mb-3 text-sm tracking-tight text-gray-700'>{blog_data[0].description}</p>
+            <h5 className='mb-2 tetx-lg font-medium tracking-tight text-grey-900'>{title}</h5>
+            <p className='mb-3 text-sm tracking-tight text-gray-700'>{description}</p>
+            <div className='inline-flex items-center py-2 font-semibold text-center '>
+                Read more<Image src={assets.arrow} className='ml-2 ' width={12}/>  
+            </div>
         </div>
     </div>
   )
